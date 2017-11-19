2682366b5eda3e2bb24b544a57fb31658a95f9ce
d p <980853230@qq.com>
2017-11-19 11:25:44 +0800

    增加写入前景视频，QJ.avi

--- opencv_dc_mfc/BJC_EXCEL.cpp
+++ opencv_dc_mfc/BJC_EXCEL.cpp
@@ -15,10 +15,9 @@ int testBJC()
 	Mat background;
 	Mat temp;
 	Mat result;
-
 	int FrameCount = 0;
 	VideoCapture capture("2.mp4");
-
+	int size;
 	if (!capture.isOpened())
 	{
 		cout << "读取视频有误" << endl;
@@ -26,6 +25,8 @@ int testBJC()
 	}
 	else
 	{
+
+		int fps = (int)capture.get(CAP_PROP_FPS);
 		long totalFrameNumber = capture.get(CV_CAP_PROP_FRAME_COUNT);//获取视频的总帧数
 		capture.read(background);
 		resize(background, background, Size(300, 200));//resize为300*200的图像，原来是720*480
@@ -34,6 +35,8 @@ int testBJC()
 		ofstream File;//ofstream类实现向文件写内容
 		File.open("test.xls", ios::_Noreplace | fstream::trunc);//ios::_Noreplace 打开一个文件时，如果文件不存在，创建该文件
 		vector<int>ivec(totalFrameNumber);//可变数组ivec，大小为总帧
+
+		VideoWriter writer("QJ.avi", VideoWriter::fourcc('M', 'P', '4', '2'), fps, Size(300, 200), 0);
 
 		while (capture.read(temp))
 		{
@@ -58,6 +61,8 @@ int testBJC()
 					}
 				}
 			}
+			result.convertTo(result, CV_8U);
+			writer << result;
 			File << PixelCount << endl;	//写入excel
 		}
 
